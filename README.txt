1d63d293985d07ae3f312cdbc7a7d2bf8f702953
Michael Gruenewald <mail@michaelgruenewald.eu>
2010-09-06 23:15:14 +0200

    license, readme and setup.py for distutils

---
+++ README.txt
@@ -0,0 +1,1 @@
+This is an API package for rememberthemilk.com.
---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='RtmAPI',
+    author='Michael Gruenewald',
+    author_email='mail@michaelgruenewald.eu',
+    url='https://bitbucket.org/michaelgruenewald/rtmapi',
+    version='0.1dev',
+    packages=['rtmapi',],
+    license='License :: OSI Approved :: MIT License',
+    long_description=open('README.txt').read(),
+)
