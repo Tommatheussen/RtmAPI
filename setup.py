190c7f8d0ae83b6611d8caab7270cb97fd4ddf0f
Michael Gruenewald <mail@michaelgruenewald.eu>
2010-09-10 15:40:14 +0200

    adding description

--- setup.py
+++ setup.py
@@ -4,10 +4,11 @@ setup(
     name='RtmAPI',
     author='Michael Gruenewald',
     author_email='mail@michaelgruenewald.eu',
+    description='API package for rememberthemilk.com',
+    long_description=open('README.txt').read(),
+    license='License :: OSI Approved :: MIT License',
     url='https://bitbucket.org/michaelgruenewald/rtmapi',
     version='0.2dev',
     packages=['rtmapi',],
-    license='License :: OSI Approved :: MIT License',
-    long_description=open('README.txt').read(),
     requires=['httplib2 (>=0.6.0)'],
 )
