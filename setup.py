aaf84a897093994c913accf1f7a2b840eb0a4812
Michael Gruenewald <mail@michaelgruenewald.eu>
2010-09-14 01:01:49 +0200

    fixing README problems

---
+++ setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+setup(
+    name='RtmAPI',
+    author='Michael Gruenewald',
+    author_email='mail@michaelgruenewald.eu',
+    description='API package for rememberthemilk.com',
+    long_description=open('README').read(),
+    license='License :: OSI Approved :: MIT License',
+    url='https://bitbucket.org/michaelgruenewald/rtmapi',
+    version='0.3',
+    packages=['rtmapi',],
+    requires=['httplib2 (>=0.6.0)'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+    ],
+)
