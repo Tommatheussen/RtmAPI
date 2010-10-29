13a86e181449c372dad4d97e0f333aae88b8e9e7
Marcin Kasperski <Marcin Kasperski@localhost>
2010-10-29 15:54:08 +0200

    Better error handling. Instead of       AssertionError there is:       rtmapi.RtmException: Request rtm.tasks.setURL failed. Status: 404, reason: Not Found

---
+++ examples/createtask.py
@@ -0,0 +1,46 @@
+import sys
+import webbrowser
+import xml.etree.ElementTree as ElementTree
+from rtmapi import Rtm
+
+if __name__ == '__main__':
+    # call the program as `listtasks.py api_key shared_secret [optional: token]`
+    # get those parameters from http://www.rememberthemilk.com/services/api/keys.rtm
+    api_key, shared_secret = sys.argv[1:3]
+    token = sys.argv[3] if len(sys.argv) >= 4 else None
+    api = Rtm(api_key, shared_secret, "delete", token)
+    
+    # authenication block, see http://www.rememberthemilk.com/services/api/authentication.rtm
+    # check for valid token
+    if not api.token_valid():
+        # use desktop-type authentication
+        url, frob = api.authenticate_desktop()
+        # open webbrowser, wait until user authorized application
+        webbrowser.open(url)
+        raw_input("Continue?")
+        # get the token for the frob
+        api.retrieve_token(frob)
+        # print out new token, should be used to initialize the Rtm object next time
+        # (a real application should store the token somewhere)
+        print "New token: %s" % api.token
+    
+    # all updates require timeline (which =~ savepoint to which
+    # one can rollback)
+    result = api.rtm.timelines.create()
+    timeline = result.timeline.value
+
+    # Create new list
+    result = api.rtm.lists.add(timeline = timeline,
+                               name = u"Zażółć gęślą test")
+    list_id = result.list.id
+    print "Created list with id", list_id
+
+    # And task
+    result = api.rtm.tasks.add(timeline = timeline,
+                               list_id = list_id,
+                               name = u"Żółć gęśą zajaźniam")
+    task_id = result.list.taskseries.task.id
+    print task_id
+    print "Created task", task_id
+
+
---
+++ examples/listtasks.py
@@ -0,0 +1,31 @@
+import sys
+import webbrowser
+import xml.etree.ElementTree as ElementTree
+from rtmapi import Rtm
+
+if __name__ == '__main__':
+    # call the program as `listtasks.py api_key shared_secret [optional: token]`
+    # get those parameters from http://www.rememberthemilk.com/services/api/keys.rtm
+    api_key, shared_secret = sys.argv[1:3]
+    token = sys.argv[3] if len(sys.argv) >= 4 else None
+    api = Rtm(api_key, shared_secret, "delete", token)
+    
+    # authenication block, see http://www.rememberthemilk.com/services/api/authentication.rtm
+    # check for valid token
+    if not api.token_valid():
+        # use desktop-type authentication
+        url, frob = api.authenticate_desktop()
+        # open webbrowser, wait until user authorized application
+        webbrowser.open(url)
+        raw_input("Continue?")
+        # get the token for the frob
+        api.retrieve_token(frob)
+        # print out new token, should be used to initialize the Rtm object next time
+        # (a real application should store the token somewhere)
+        print "New token: %s" % api.token
+    
+    # get all open tasks, see http://www.rememberthemilk.com/services/api/methods/rtm.tasks.getList.rtm
+    result = api.rtm.tasks.getList(filter="status:incomplete")
+    for tasklist in result.tasks:
+        for taskseries in tasklist:
+            print taskseries.task.due, taskseries.name
