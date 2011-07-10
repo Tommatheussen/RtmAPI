eb3e5a4028977100fa9e870b2dd804cde1faad0d
Michael Gruenewald <mail@michaelgruenewald.eu>
2011-07-10 11:52:16 +0200

    Python 3 support via distribute's 2to3 support

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='RtmAPI',
@@ -8,7 +8,7 @@ setup(
     long_description=open('README').read(),
     license='License :: OSI Approved :: MIT License',
     url='https://bitbucket.org/michaelgruenewald/rtmapi',
-    version='0.4',
+    version='0.5',
     packages=['rtmapi',],
     requires=['httplib2 (>=0.6.0)'],
     classifiers=[
@@ -16,6 +16,11 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.1',
     ],
+    use_2to3 = True,
 )
