fbc665d25ec54489fa7f8e384c4239dbd2a42c00
Michael Gruenewald <mail@michaelgruenewald.eu>
2011-07-14 23:57:10 +0200

    0.5.1 maintenance release to make python3 linux/osx (and pypm) happy

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 from setuptools import setup
+import codecs
 
 setup(
     name='RtmAPI',
     author='Michael Gruenewald',
     author_email='mail@michaelgruenewald.eu',
     description='API package for rememberthemilk.com',
-    long_description=open('README').read(),
+    long_description=codecs.getreader('utf-8')(open('README', 'rb')).read(),
     license='License :: OSI Approved :: MIT License',
     url='https://bitbucket.org/michaelgruenewald/rtmapi',
-    version='0.5',
+    version='0.5.1',
     packages=['rtmapi',],
     requires=['httplib2 (>=0.6.0)'],
     classifiers=[
