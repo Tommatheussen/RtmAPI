9aee4d2c513acdaa0eb41cb52969d384d57e04ae
Maxim Velikobratov <jaropolk@gmail.com>
2017-01-21 14:03:32 +0000

    version 0.7.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     long_description=open('README').read(),
     license='License :: OSI Approved :: MIT License',
     url='https://bitbucket.org/michaelgruenewald/rtmapi',
-    version='0.7.0',
+    version='0.7.1',
     packages=['rtmapi',],
     requires=['httplib2 (>=0.6.0)'],
     classifiers=[
